16bcf09bdf80f111bf4e387d239b399dfe61a5de
html-generator@hp.com <html-generator@hp.com@0eb0fa8a-c87d-11dd-87b6-9d1a99559f42>
2008-12-20 07:42:41 +0000

    Added some Gagawa examples to com.hp.gagawa.examples and added a compile.examples target to build.xml.

--- trunk/gagawa/examples/com/hp/gagawa/examples/SimpleGagawaExample.java
+++ trunk/gagawa/examples/com/hp/gagawa/examples/SimpleGagawaExample.java
@@ -20,37 +20,48 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-package com.hp.gagawa;
+package com.hp.gagawa.examples;
 
 import com.hp.gagawa.elements.Body;
-import com.hp.gagawa.elements.Doctype;
+import com.hp.gagawa.elements.H1;
 import com.hp.gagawa.elements.Head;
 import com.hp.gagawa.elements.Html;
+import com.hp.gagawa.elements.Text;
+import com.hp.gagawa.elements.Title;
 
+/**
+ * Uses Gagawa to build a very very simple web-page.
+ * Demonstrates the usage of Html, Head, Title, Body,
+ * and H1.
+ * @author kolichko Mark Kolich
+ *
+ */
+public class SimpleGagawaExample {
 
+	/**
+	 * @param args
+	 */
+	public static void main ( String [] args ) {
+		
+		Html html = new Html();
+		Head head = new Head();
+		
+		html.appendChild( head );
+		
+		Title title = new Title();
+		title.appendChild( new Text("Example Page Title") );
+		head.appendChild( title );
+		
+		Body body = new Body();
+		
+		html.appendChild( body );
+		
+		H1 h1 = new H1();
+		h1.appendChild( new Text("Example Page Header") );
+		body.appendChild( h1 );
+		
+		System.out.println( html.write() );
 
-
-public class Document{
-
-	private Doctype doctype;
-	private Html html;
-	public Head head;
-	public Body body;
-	
-	public Document(DocumentType spec) {
-		doctype = new Doctype(spec);
-		html = new Html();
-		head = new Head();
-		body = new Body();
-		html.appendChild(head);
-		html.appendChild(body);
-	}
-	
-	public String write(){
-		StringBuffer b = new StringBuffer();
-		b.append(doctype.write());
-		b.append(html.write());
-		return b.toString();
 	}
 
 }
