7176c7d2701bb58f2e20d30940a26374327c74dd
html-generator@hp.com <html-generator@hp.com@0eb0fa8a-c87d-11dd-87b6-9d1a99559f42>
2008-12-26 19:22:10 +0000

    Added inital version of PHPBuilder and all of the PHP classes that were built from PHPBuilder into src/com/hp/gagawa/php/elements/.  Run Example.php under src/com/hp/gagawa/php/ to watch the magic happen.

--- trunk/gagawa/builder/com/hp/gagawa/builder/php/PHPBuilder.java
+++ trunk/gagawa/builder/com/hp/gagawa/builder/php/PHPBuilder.java
@@ -22,10 +22,93 @@ THE SOFTWARE.
 
 package com.hp.gagawa.builder.php;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.xerces.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.hp.gagawa.builder.php.Tag;
+
+
+/**
+ * The PHPBuilder builds the PHP file for each tag and places
+ * the PHP classes into src/com/hp/gagawa/php/elements.
+ * @author kolichko
+ *
+ */
 public class PHPBuilder {
 	
-	public static void main ( String [] args ) {
+	public static void main(String[] args) {
 		
+		String myTag = null;
+		if(args.length > 0){
+			myTag = args[0];
+		}
+		
+		DOMParser parser = new DOMParser();
+		try {
+			File xml = new File("builder/com/hp/gagawa/builder/tags.xml");
+			parser.parse(xml.getCanonicalPath());
+			Document doc = parser.getDocument();
+			
+			NodeList nodes = doc.getElementsByTagName("tag");
+			int ilen = nodes.getLength();
+			for(int i = 0; i < ilen; i++){
+				Node node = nodes.item(i);
+				NamedNodeMap nnm = node.getAttributes();
+				String name = "";
+				boolean allowKids = false;
+				if(nnm != null){
+					name = nnm.getNamedItem("name").getNodeValue();
+					allowKids = nnm.getNamedItem("allowsKids") == null;
+				}
+				ArrayList<String> req = new ArrayList<String>();
+				ArrayList<String> opt = new ArrayList<String>();
+				NodeList kids = node.getChildNodes();
+				int jlen = kids.getLength();
+				NodeList attrs = null;
+				for(int j = 0; j < jlen; j++){
+					if(kids.item(j).getNodeName().equals("attributes")){
+						attrs = kids.item(j).getChildNodes();
+						break;
+					}
+				}
+				if(attrs != null){
+					jlen = attrs.getLength();
+					for(int j = 0; j < jlen; j++){
+						Node attr = attrs.item(j);
+						NamedNodeMap am = attr.getAttributes();
+						if(am != null){
+							String nodeValue = attr.getFirstChild().getNodeValue();
+							if(am.getNamedItem("required") != null){
+								req.add(nodeValue);
+							}else{
+								opt.add(nodeValue);
+							}
+						}
+					}
+				}
+				if(myTag == null || myTag.equalsIgnoreCase(name)){
+					new Tag(name, allowKids, req, opt).writeTagClass();
+				}
+			}
+			
+			
+		} catch (SAXException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("Complete!");
+		System.exit(0);
 	}
 
 }
